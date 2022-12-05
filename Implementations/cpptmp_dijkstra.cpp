5714099f732ed4d2fbdf3d872e8b837210691e08
Botao Yuan <102369867+aqxa2k@users.noreply.github.com>
2022-12-04 20:46:33 -0600

    Add files via upload

---
+++ Implementations/cpptmp_dijkstra.cpp
@@ -0,0 +1,39 @@
+/** 
+ * Description: Finds shortest path from node x to all other nodes
+ * Verification: https://open.kattis.com/problems/shortestpath1
+ * Time: O(M log N) 
+**/
+
+template <class T> 
+class dijkstra {
+    const long long __inf_d = 9e18; 
+    public: 
+        int n; 
+        vector<T> d; 
+        vector<vector<pair<int, T>>> g; 
+        dijkstra(int _n) {
+            n = _n; 
+            d = vector<T>(n); 
+            g = vector<vector<pair<int, T>>>(n); 
+        }
+        inline void add(int u, int v, T w, bool dir = 0) {
+            g[u].push_back({v, w}); 
+            if (dir) g[v].push_back({v, w}); 
+        }
+        void run(int st = 0) {
+            for (int i = 0; i < n; ++i) d[i] = __inf_d; 
+            priority_queue<pair<T, int>, vector<pair<T, int>>, greater<pair<T, int>>> q; 
+            d[st] = 0; q.push({0, st}); 
+            while (!q.empty()) {
+                pair<T, int> p = q.top();  q.pop(); 
+                T dist = p.first; int u = p.second; 
+                for (pair<T, int> next: g[u]) {
+                    int v = next.first; T next_dist = dist + next.second; 
+                    if (next_dist < d[v]) {
+                        d[v] = next_dist; q.push({next_dist, v}); 
+                    }
+                }
+            }
+        }
+        inline T get(int node) { if (d[node] == __inf_d) return -1; return d[node]; } 
+}; 
