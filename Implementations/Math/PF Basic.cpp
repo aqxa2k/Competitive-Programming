21e639e276b223a94da57cdd745e230bb790d2d9
Botao Yuan <102369867+aqxa2k@users.noreply.github.com>
2022-12-05 15:06:35 -0600

    Update PF Basic.cpp

--- Implementations/Math/PF Basic.cpp
+++ Implementations/Math/PF Basic.cpp
@@ -1,9 +1,22 @@
+//not finished
 //add stuff
-template <class T> vector<T, int> factor_basic(T x) {
-  vector<T, int> ret; 
-  
+
+
+template <class T> vector<pair<T, int>> factor_basic(T x) {
+  vector<pair<T, int>> ret; 
+  if (x % 2 == 0) {
+	ret.push_back({2, 0});
+	while (x % 2 == 0) ret.back().second++, x /= 2; 
+  }
+  for (T i = 3; i * i <= x; ++i) {
+	  if (x % i == 0) {
+	    ret.push_back({i, 0});
+      while (x % i == 0) {
+        ret.back().second()++, x /= i; 
+    }	
+  }
+  if (x > 2) ret.push_back({x, 1}); 
   return ret; 
 }
 
 #define factor factor_basic
-
