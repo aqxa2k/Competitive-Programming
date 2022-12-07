588bac94b58e9e6648102001940e6a5800d30566
Botao Yuan <102369867+aqxa2k@users.noreply.github.com>
2022-12-07 08:01:01 -0600

    Update SegtreeBasic

--- Implementations/Data-Structures/SegtreeBasic.cpp
+++ Implementations/Data-Structures/SegtreeBasic.cpp
@@ -3,24 +3,23 @@
 
 //Segtree: initialize, make, build, modify, query， incremenet; 
 template <class T> 
-class Segtree {
-    private: 
+class Segtree { 
+    public:
     vector<T> t;
     int n; T init = 0; //change initial value! 
     T op(T a, T b) {
         return a + b; //change operation!
-    } 
-    public:
-    Segtree(T N, vector<T> v = {}) { 
-        t = vector<T>(2 * N, init); 
-        n = N; 
-		if (v.size()) make(v); 
     }
     void make(vector<T> & a) { 
         for (int i = 0; i < n; ++i) t[n+i] = a[i]; 
     } 
     void build() {
         for (int i = n - 1; i > 0; --i) t[i] = op(t[i<<1], t[i<<1|1]);
+    }
+    Segtree(T N, vector<T> v = {}) { 
+        t = vector<T>(2 * N, init); 
+        n = N; 
+	if (v.size()) { make(v); build(); }  
     }
     void modify(int p, T val) {
         for (t[p += n] = val; p > 1; p >>= 1) t[p>>1] = op(t[p], t[p^1]);
